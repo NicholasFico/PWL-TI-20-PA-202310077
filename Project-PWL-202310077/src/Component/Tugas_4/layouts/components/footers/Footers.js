fe511411f09bc993792105c354242c0ba3078006
NicholasFico <92150095+NicholasFico@users.noreply.github.com>
2023-04-14 13:10:27 +0700

    Tugas_4

---
+++ Project-PWL-202310077/src/Component/Tugas_4/layouts/components/footers/Footers.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footers() {
+    return (
+        <footer className="container">
+            <p>&copy; 2023 Nicholas F, Inc. &middot; 
+            <a href="https://wa.me/087874544493" className='ms-1' target={"_blank"}>Contact</a></p>
+        </footer>
+    )
+}
